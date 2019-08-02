6d4e71160a40001b2cc26eeb82b0c563683dfc5e
FreeCodeUser <FreeWebUser@protonmail.com>
2019-08-02 20:46:27 +0200

    improved program,added elif statement

--- json_app.py
+++ json_app.py
@@ -1,13 +1,16 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(w):
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Did you mean {} instead?".format(get_close_matches(w, data.keys())[0])
     else:
         return "The {} is not in library".format(w)
 
-word = input("Enter word: ")
+word = input("Enter word: ").lower()
 
 print(translate(word))
