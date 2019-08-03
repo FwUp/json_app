3741ab4c7f9b8f5456d82cf7fd7b379be7e44117
FreeCodeUser <FreeWebUser@protonmail.com>
2019-08-03 12:35:48 +0200

    finished course content

--- json_app.py
+++ json_app.py
@@ -4,13 +4,30 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def translate(w):
+    w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
-        return "Did you mean {} instead?".format(get_close_matches(w, data.keys())[0])
+        answer = input("Did you mean {} instead?\nEnter Y if yes and N if no: ".format(get_close_matches(w, data.keys())[0]))
+        if answer == "Y" or answer == "y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif answer == "N" or answer == "n":
+            return "The \"{}\" doesn't exist in dictionary.Please double check it.".format(word)
+        else:
+            return "We didn't understand your entry."
     else:
-        return "The {} is not in library".format(w)
+        return "The \"{}\" is not in dictionary".format(w)
 
-word = input("Enter word: ").lower()
+word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for i in output:
+        print(i)
+else:
+    print(output)
