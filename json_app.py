68d1ea8fdc43754b85f514800c042bbd06a4eabc
FreeCodeUser <FreeWebUser@protonmail.com>
2019-08-03 12:49:37 +0200

    add number elif and comments

--- json_app.py
+++ json_app.py
@@ -1,17 +1,19 @@
 import json
 from difflib import get_close_matches
 
+# load json data
 data = json.load(open("data.json"))
 
+# main function
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
     elif w.title() in data:
         return data[w.title()]
-    elif w.upper() in data:
+    elif w.upper() in data: # check acronyms
         return data[w.upper()]
-    elif len(get_close_matches(w, data.keys())) > 0:
+    elif len(get_close_matches(w, data.keys())) > 0: # check close matches
         answer = input("Did you mean {} instead?\nEnter Y if yes and N if no: ".format(get_close_matches(w, data.keys())[0]))
         if answer == "Y" or answer == "y":
             return data[get_close_matches(w, data.keys())[0]]
@@ -19,6 +21,8 @@ def translate(w):
             return "The \"{}\" doesn't exist in dictionary.Please double check it.".format(word)
         else:
             return "We didn't understand your entry."
+    elif  w.isdigit(): # check numbers
+        return "That's a number fool,only words are allowed"
     else:
         return "The \"{}\" is not in dictionary".format(w)
 
@@ -26,7 +30,7 @@ word = input("Enter word: ")
 
 output = translate(word)
 
-if type(output) == list:
+if type(output) == list: # check if output is list data type
     for i in output:
         print(i)
 else:
